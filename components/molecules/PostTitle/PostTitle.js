9af52b1e536ebde8d3c3be4c8ad1a0b5c6a2698c
Taka Harada <harada@hivelocity.co.jp>
2019-10-04 17:28:33 +0900

    Load some values from API

--- components/molecules/PostTitle/PostTitle.js
+++ components/molecules/PostTitle/PostTitle.js
@@ -1,37 +1,51 @@
-import React, { Component } from 'react'
-import Moment from 'react-moment'
+import React, { Component } from "react";
+import Link from "next/link";
+import Moment from "react-moment";
 
-import './PostTitle.scss'
+import "./PostTitle.scss";
 
 class PostTitle extends Component {
-
   render() {
-
-    // console.log('start PostTitle')
+    // console.log("start PostTitle");
     // console.log(this.props);
-    // console.log('end PostTitle')
+    // console.log("end PostTitle");
 
     return (
       <header className="p-article__header">
         <h1>{this.props.post.title.rendered}</h1>
         <ul className="post-date">
           <li className="date">
-            <Moment format="YYYY.MM.DD">{this.props.post.date}</Moment></li>
-          <li className="update">
-            <Moment format="YYYY.MM.DD">
-            {this.props.post.modified}
-            </Moment>
-            <span>Update</span>
+            <Moment format="YYYY.MM.DD">{this.props.post.date}</Moment>
           </li>
+          {this.props.post.modified && (
+            <li className="update">
+              <Moment format="YYYY.MM.DD">{this.props.post.modified}</Moment>
+              <span>Update</span>
+            </li>
+          )}
         </ul>
         <div className="eyecatch">
           <div className="eyecatch-image">
-            <div className="img-box"><img src={this.props.post.source_url} /></div>
-            <div className="badge-category badge-ancientcity"><a href="#">日本の古都</a></div>
+            <div className="img-box">
+              <img src={this.props.post.featured_image} />
+            </div>
+            {this.props.post.jp_category && (
+              <div className="badge-category badge-ancientcity">
+                <Link
+                  as={`/jp/${this.props.post.jp_category[0].slug}`}
+                  href={{
+                    pathname: "/categoryJp",
+                    query: { slug: this.props.post.jp_category[0].slug }
+                  }}
+                >
+                  <a>{this.props.post.jp_category[0].name}</a>
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       </header>
-    )
+    );
   }
 }
 
