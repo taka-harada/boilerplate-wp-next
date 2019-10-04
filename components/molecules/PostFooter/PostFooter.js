f33d146f8234a494eb594ac2e2f7beed2c4af8aa
Taka Harada <harada@hivelocity.co.jp>
2019-10-04 18:06:43 +0900

    Fix logic to display TagBoxx component. If some posts doesn't has any tag, it's not display.

--- components/molecules/PostFooter/PostFooter.js
+++ components/molecules/PostFooter/PostFooter.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import PostTagbox from '../PostTagbox/PostTagbox'
-import PostAuthorbox from '../PostAuthorbox/PostAuthorbox'
-import PostImage from '../../../static/PostImage'
-import './PostFooter.scss'
+import PostTagbox from "../PostTagbox/PostTagbox";
+import PostAuthorbox from "../PostAuthorbox/PostAuthorbox";
+import PostImage from "../../../static/PostImage";
+import "./PostFooter.scss";
 
 class PostFooter extends Component {
-
   render() {
-
+    console.log("start postFooter");
+    console.log(this.props);
+    console.log("end postFooter");
     return (
       <footer className="p-article__footer">
-
-        <PostTagbox />
-        <PostAuthorbox />
-
+        {this.props.post.common_tag && <PostTagbox {...this.props} />}
+        <PostAuthorbox {...this.props} />
       </footer>
-    )
+    );
   }
 }
 
