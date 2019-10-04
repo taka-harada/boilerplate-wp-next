52066a7160334986c3e63a6d45a55e1dbf3dfd6e
Taka Harada <harada@hivelocity.co.jp>
2019-10-04 18:02:04 +0900

    Fix some code to get some values from API

--- components/molecules/PostTagbox/PostTagbox.js
+++ components/molecules/PostTagbox/PostTagbox.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Link from "next/link";
 
-import './PostTagbox.scss'
+import "./PostTagbox.scss";
 
 class PostTagbox extends Component {
-
   render() {
+    const tags = this.props.post.common_tag.map(tag => (
+      <li key={tag.term_id}>
+        <Link
+          as={`/tag/${tag.slug}`}
+          href={{ pathname: "/tag", query: { slug: tag.slug } }}
+        >
+          <a>{tag.name}</a>
+        </Link>
+      </li>
+    ));
+    // console.log("start tagbox");
+    // console.log(this.props);
+    // console.log(tags);
+    // console.log("end tagbox");
 
     return (
       <section className="p-articlefoot-tags">
-        <ul>
-          <li><a href="#">和食</a></li>
-          <li><a href="#">日本食</a></li>
-        </ul>
+        <ul>{tags}</ul>
       </section>
-    )
+    );
   }
 }
 
