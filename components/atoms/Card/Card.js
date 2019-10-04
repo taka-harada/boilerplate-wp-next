1816c4481651a2552ab7d1eaf0351887852daa12
Taka Harada <harada@hivelocity.co.jp>
2019-10-04 18:53:01 +0900

    Change logic to display somme values of category name via API

--- components/atoms/Card/Card.js
+++ components/atoms/Card/Card.js
@@ -1,75 +1,172 @@
-import React, { Component } from 'react'
-import Link from 'next/link'
+import React, { Component } from "react";
+import Link from "next/link";
 
-import PostImage from '../../../static/PostImage'
-import './Card.scss'
+import PostImage from "../../../static/PostImage";
+import "./Card.scss";
 
 class Card extends Component {
-
   renderTopCard() {
     return (
-      <article className={this.props.lcardFlg ? 'c-card c-card--l' : 'c-card c-card--s' }>
+      <article
+        className={
+          this.props.lcardFlg ? "c-card c-card--l" : "c-card c-card--s"
+        }
+      >
         <div className="c-card__img">
-          <div className="img-box"><Link as={`/jp/${this.props.id}`} href={{pathname: '/postJp', query: { id: this.props.id }}}><a><img src={this.props.source_url} alt="eyecatch image" /></a></Link></div>
-          <div className="badge-category badge-ancientcity"><a href="#">日本の古都</a></div>
+          <div className="img-box">
+            <Link
+              as={`/jp/${this.props.id}`}
+              href={{ pathname: "/postJp", query: { id: this.props.id } }}
+            >
+              <a>
+                <img src={this.props.featured_image} alt="eyecatch image" />
+              </a>
+            </Link>
+          </div>
+          {this.props.jp_category && (
+            <div className="badge-category badge-ancientcity">
+              <Link
+                as={`/jp/${this.props.jp_category[0].slug}`}
+                href={{
+                  pathname: "/categoryJp",
+                  query: { slug: this.props.slug }
+                }}
+              >
+                <a>{this.props.jp_category[0].name}</a>
+              </Link>
+            </div>
+          )}
         </div>
         <div className="c-card__txt">
           <div className="txt-box">
-            <h2><Link as={`/jp/${this.props.id}`} href={{pathname: '/postJp', query: { id: this.props.id }}}><a>{this.props.title.rendered}</a></Link></h2>
+            <h2>
+              <Link
+                as={`/jp/${this.props.id}`}
+                href={{ pathname: "/postJp", query: { id: this.props.id } }}
+              >
+                <a>{this.props.title.rendered}</a>
+              </Link>
+            </h2>
           </div>
         </div>
       </article>
-    )
+    );
   }
 
   renderArchiveCard() {
     return (
-      <article className={this.props.lcardFlg ? 'c-card c-card--archive-row' : 'c-card c-card--archive-column' }>
+      <article
+        className={
+          this.props.lcardFlg
+            ? "c-card c-card--archive-row"
+            : "c-card c-card--archive-column"
+        }
+      >
         <div className="c-card__img">
-          <div className="img-box"><Link as={`/jp/${this.props.id}`} href={{pathname: '/postJp', query: { id: this.props.id }}}><a><img src={this.props.source_url} alt="eyecatch image" /></a></Link></div>
-          <div className="badge-category badge-ancientcity"><a href="#">日本の古都</a></div>
+          <div className="img-box">
+            <Link
+              as={`/jp/${this.props.id}`}
+              href={{ pathname: "/postJp", query: { id: this.props.id } }}
+            >
+              <a>
+                <img src={this.props.featured_image} alt="eyecatch image" />
+              </a>
+            </Link>
+          </div>
+          {this.props.jp_category && (
+            <div className="badge-category badge-ancientcity">
+              <Link
+                as={`/jp/tag/${this.props.slug}`}
+                href={{
+                  pathname: "/categoryJp",
+                  query: { slug: this.props.slug }
+                }}
+              >
+                <a>{this.props.jp_category[0].name}</a>
+              </Link>
+            </div>
+          )}
         </div>
         <div className="c-card__txt">
           <div className="txt-box">
-            <h2><Link as={`/jp/${this.props.id}`} href={{pathname: '/postJp', query: { id: this.props.id }}}><a>{this.props.title.rendered}</a></Link></h2>
+            <h2>
+              <Link
+                as={`/jp/${this.props.id}`}
+                href={{ pathname: "/postJp", query: { id: this.props.id } }}
+              >
+                <a>{this.props.title.rendered}</a>
+              </Link>
+            </h2>
           </div>
         </div>
       </article>
-    )
+    );
   }
 
   renderPopularCard() {
     return (
-      <article className={this.props.lcardFlg ? 'c-card c-card--reco-l' : 'c-card c-card--reco-s' }>
+      <article
+        className={
+          this.props.lcardFlg
+            ? "c-card c-card--reco-l"
+            : "c-card c-card--reco-s"
+        }
+      >
         <div className="c-card__img">
-          <div className="img-box"><Link as={`/jp/${this.props.id}`} href={{pathname: '/postJp', query: { id: this.props.id }}}><a><img src={this.props.source_url} alt="eyecatch image" /></a></Link></div>
-          <div className="badge-category badge-ancientcity"><a href="#">日本の古都</a></div>
+          <div className="img-box">
+            <Link
+              as={`/jp/${this.props.id}`}
+              href={{ pathname: "/postJp", query: { id: this.props.id } }}
+            >
+              <a>
+                <img src={this.props.featured_image} alt="eyecatch image" />
+              </a>
+            </Link>
+          </div>
+          {this.props.jp_category && (
+            <div className="badge-category badge-ancientcity">
+              <Link
+                as={`/jp/${this.props.jp_category[0].slug}`}
+                href={{
+                  pathname: "/categoryJp",
+                  query: { slug: this.props.slug }
+                }}
+              >
+                <a>{this.props.jp_category[0].name}</a>
+              </Link>
+            </div>
+          )}
         </div>
         <div className="c-card__txt">
           <div className="txt-box">
-            <h2><Link as={`/jp/${this.props.id}`} href={{pathname: '/postJp', query: { id: this.props.id }}}><a>{this.props.title.rendered}</a></Link></h2>
+            <h2>
+              <Link
+                as={`/jp/${this.props.id}`}
+                href={{ pathname: "/postJp", query: { id: this.props.id } }}
+              >
+                <a>{this.props.title.rendered}</a>
+              </Link>
+            </h2>
           </div>
         </div>
       </article>
-    )
+    );
   }
 
   render() {
+    const currentRoute = this.props.route;
 
-    const currentRoute = this.props.route
+    // console.log("start categoryInfo card");
+    // console.log(this.props.jp_category);
+    // console.log("end card");
 
-    // console.log('start card')
-    // console.log(this.props)
-    // console.log('end card')
-
-    switch(currentRoute){
-      case 'archive':
-        return this.renderArchiveCard()
-      case 'popular':
-        return this.renderPopularCard()
+    switch (currentRoute) {
+      case "archive":
+        return this.renderArchiveCard();
+      case "popular":
+        return this.renderPopularCard();
       default:
-        return this.renderTopCard()
-
+        return this.renderTopCard();
     }
   }
 }
