637fb97c4934fe3db93266db6e7f34ab813ede44
Taka Harada <harada@hivelocity.co.jp>
2019-10-25 18:47:16 +0900

    Add category archive component

---
+++ components/molecules/ListPostBlogCategory/ListPostBlogCategory.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+
+import fetch from 'isomorphic-unfetch'
+//import { url, postType } from '../../../Api/Api'
+import { category, url, postType } from '../../../Api/Api'
+import Card from '../../atoms/Card/Card'
+
+class ListPostBlogCategory extends Component {
+  constructor() {
+    super();
+    this.isLoading = false;
+    this.fetchData = this.fetchData.bind(this);
+    this.renderPost = this.renderPost.bind(this);
+    this.state = {
+      data: [],
+      isLoaded: false
+    }
+  }
+
+  fetchData() {
+    if (this.isLoading === false) {
+      let path = this.props.route.asPath
+
+      return category(this.props.route.asPath, 1).then(res => res.json())
+        .then(data => {
+          this.setState({
+            data: data,
+            isLoaded: true,
+            isLoading: false
+          })
+        })
+    }
+  }
+
+  componentDidMount() {
+
+    //1回目のfetch
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.route.asPath !== prevProps.route.asPath) {
+
+      // 2回目以降のfetch
+      this.fetchData();
+
+    }
+  }
+
+  renderPost() {
+
+    const postMaxNum = 10;
+    const postNum = this.state.data.total
+    const arrPost = this.state.data.posts.slice(0, postNum);
+
+    const blogPost = arrPost.map((item, index) => {
+      return (
+        <Card {...item} key={item.id} lcardFlg={index < 2} route={'archive'} />
+      )
+    });
+
+    if (blogPost.length > 0) {
+      return blogPost
+    } else {
+      return (
+        <p>関連記事はありません。</p>
+      )
+    }
+  }
+
+  render() {
+
+    if (this.state.isLoaded) {
+      return this.renderPost();
+    } else {
+      return <p>spiner</p>
+    }
+  }
+
+}
+
+export default ListPostBlogCategory;
