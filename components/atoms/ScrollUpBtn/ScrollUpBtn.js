45d7a3b6cf5f44c65a2b0137669921237e19c5b9
Taka Harada <harada@hivelocity.co.jp>
2019-12-05 21:17:30 +0900

    Add function for hiding scrollTopButton

--- components/atoms/ScrollUpBtn/ScrollUpBtn.js
+++ components/atoms/ScrollUpBtn/ScrollUpBtn.js
@@ -1,21 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { animateScroll as scroll } from 'react-scroll'
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 
 import './ScrollUpBtn.scss'
 
-class ScrollUpBtn extends Component {
+const ScrollUpBtn = () => {
 
-  scrollToTop = () => {
-    scroll.scrollToTop();
-  };
+  // showOnScrollに初期フラグをtrueにセット
+  const [showOnScroll, setShowOnScroll] = useState(true)
 
-  render() {
-    return (
-      <div className="c-page-top show" onClick={this.scrollToTop}>
-        <a href="#"><img src="../../static/common/page_top_icon.svg" alt="pagetop" /></a>
-      </div>
-    );
-  }
+  useScrollPosition(({ prevPos, currPos }) => {
+
+    const isShow = currPos.y > prevPos.y
+
+    // スクロールダウンするとisShowはfalse、スクロールアップするとisShowはtrue
+    if (isShow !== showOnScroll) setShowOnScroll(isShow)
+  },
+    [showOnScroll],
+    false,
+    false,
+    300
+  )
+
+  return (
+    <div className={showOnScroll ? "c-page-top" : "c-page-top show"} onClick={event => { scroll.scrollToTop() }}>
+      <a href="#"><img src="../../static/common/page_top_icon.svg" alt="pagetop" /></a>
+    </div>
+  )
+
 }
+
+// class ScrollUpBtn extends Component {
+
+//   constructor(props) {
+//     super(props)
+//     this.node = React.createRef()
+//   }
+
+//   state = {
+//     scrollTop: 0
+//   }
+
+//   scrollToTop = () => {
+//     scroll.scrollToTop();
+//   };
+
+//   render() {
+//     return (
+//       <div className="c-page-top show" onClick={this.scrollToTop}>
+//         <a href="#"><img src="../../static/common/page_top_icon.svg" alt="pagetop" /></a>
+//       </div>
+//     );
+//   }
+// }
 
 export default ScrollUpBtn;
